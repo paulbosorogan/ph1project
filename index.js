4ba893dc876564a8d19d4dddf2828f9c4ead8e50
Paul Bosorogan <bosorogan.paul@gmail.com>
2022-12-27 09:40:03 +0200

    Update fetch functions to make application more interactive and eliminate repeating code

--- index.js
+++ index.js
@@ -1,55 +1,13 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-
-
-//breakfast bttn
-const breakfastBttn = document.querySelector('#br_button')
-breakfastBttn.addEventListener('click', ()=>{
-    document.querySelector('#card_collection').innerHTML=''
-     fetch('http://localhost:3000/recipes')
+let recipes
+const cardCollection = document.querySelector('#card_collection')
+function fetchRecipes(){
+    fetch('http://localhost:3000/recipes')
     .then(res=>res.json())
-  //  .then(recipes => recipes.filter(recipe => {
-   //     recipe.meal =='breakfast'
-    //    appendFood(recipe)}))
-    .then(obj=> {
-        const array = obj.filter(recipe => recipe.meal ==='breakfast')
-        array.forEach(recipe => appendFood(recipe))
+    .then(recipesArray=> {
+      recipes = recipesArray
     })
-})
-
-//lunch bttn
-const lunchBttn = document.querySelector('#lu_button')
-lunchBttn.addEventListener('click',()=>{
-    document.querySelector('#card_collection').innerHTML=''
-     fetch('http://localhost:3000/recipes')
-    .then(res=>res.json())
-    .then(obj=> {
-        const array = obj.filter(recipe => recipe.meal ==='lunch')
-        array.forEach(recipe => appendFood(recipe))})
-})
-//dinner bttn
-const dinnerBttn = document.querySelector('#dn_button')
-dinnerBttn.addEventListener('click', ()=>{
-    document.querySelector('#card_collection').innerHTML=''
-     fetch('http://localhost:3000/recipes')
-    .then(res=>res.json())
-    .then(obj=> {
-        const array = obj.filter(recipe => recipe.meal ==='dinner')
-        array.forEach(recipe => appendFood(recipe))})
-})
-//home button
-const homeButton = document.querySelector('#homeBttn')
-homeButton.addEventListener('click', ()=>{
-    document.querySelector('#card_collection').innerHTML=''
-})
-
-//submit eventListener
-const form = document.querySelector('form')
-form.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    alert("Thank you for subscribing!")
-    form.reset()
-})
-
+    
+}
 function appendFood(recipe){
     let card =document.createElement('div')
     card.className='cards'
@@ -64,6 +22,44 @@ function appendFood(recipe){
 </p>
 </div>`
 
-    document.querySelector('#card_collection').appendChild(card)
+cardCollection.appendChild(card)
 }
+function filterButtonFunction(e){
+       cardCollection.innerHTML=''
+       if(e.target.name !== "home"){
+        const array = recipes.filter(recipe => recipe.meal ===e.target.name)
+        array.forEach(recipe => appendFood(recipe))
+       }
+        
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+fetchRecipes()
+//universal callback function
+
+//breakfast bttn
+const breakfastBttn = document.querySelector('#br_button')
+breakfastBttn.addEventListener('click',filterButtonFunction)
+
+//lunch bttn
+const lunchBttn = document.querySelector('#lu_button')
+lunchBttn.addEventListener('click',filterButtonFunction)
+//dinner bttn
+const dinnerBttn = document.querySelector('#dn_button')
+dinnerBttn.addEventListener('click', filterButtonFunction)
+//new function
+
+
+//home button
+const homeButton = document.querySelector('#homeBttn')
+homeButton.addEventListener('click', filterButtonFunction)
+
+//submit eventListener
+const form = document.querySelector('form')
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    alert("Thank you for subscribing!")
+    form.reset()
+})
+
 })
