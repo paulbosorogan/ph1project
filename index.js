39d9ef81a39a84fd3c88f075d93237c58935f425
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-02-07 17:34:21 -0500

    Updated the order of the functions after the DOMContentLoaded event listener

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 let recipes
 const cardCollection = document.querySelector('#card_collection')
+//fetching recipes
 function fetchRecipes(){
     fetch('http://localhost:3000/recipes')
     .then(res=>res.json())
     .then(recipesArray=> {
       recipes = recipesArray
     })
-    
 }
+//filtering recipes from server and appending to DOM
+function filterButtonFunction(e){
+    cardCollection.innerHTML=''
+    if(e.target.name !== "home"){
+     const recipesArray = recipes.filter(recipe => recipe.meal ===e.target.name)
+     recipesArray.forEach(recipe => appendFood(recipe))
+    }
+     
+}
+
 function appendFood(recipe){
     let card =document.createElement('div')
     card.className='cards'
@@ -19,35 +29,25 @@ function appendFood(recipe){
         <p><b>Description</b>: ${recipe.description}
         <br>
         <p id="like-count">0 Likes</p>
-        <button id="like_button">Like</button>
+        <button id="like_button" class="button">Like</button>
     </div>
      <img src="${recipe.image}" alt="Photo with"">
-</p>
-</div>`
-const likeCount = card.querySelector('#like-count')
-const likeBttn = card.querySelector('#like_button')
-let counter = 0
-likeBttn.addEventListener('click', ()=>{
-    counter=counter+1
-    likeCount.textContent= counter + ' Likes'
-})
+        </p>
+    </div>`
+    //like button
+    const likeCount = card.querySelector('#like-count')
+    const likeBttn = card.querySelector('#like_button')
+    let counter = 0
+    likeBttn.addEventListener('click', ()=>{
+        counter=counter+1
+        likeCount.textContent= counter + ' Likes'
+    })
 
 cardCollection.appendChild(card)
 }
-function filterButtonFunction(e){
-       cardCollection.innerHTML=''
-       if(e.target.name !== "home"){
-        const recipesArray = recipes.filter(recipe => recipe.meal ===e.target.name)
-        recipesArray.forEach(recipe => appendFood(recipe))
-       }
-        
-}
-
-
 
 document.addEventListener('DOMContentLoaded', ()=>{
 fetchRecipes()
-//universal callback function
 
 //breakfast bttn
 const breakfastBttn = document.querySelector('#br_button')
@@ -59,14 +59,10 @@ lunchBttn.addEventListener('click',filterButtonFunction)
 //dinner bttn
 const dinnerBttn = document.querySelector('#dn_button')
 dinnerBttn.addEventListener('click', filterButtonFunction)
-//new function
-
 
 //home button
 const homeButton = document.querySelector('#homeBttn')
 homeButton.addEventListener('click', filterButtonFunction)
-
-//like bttn
 
 //submit eventListener
 const form = document.querySelector('form')
